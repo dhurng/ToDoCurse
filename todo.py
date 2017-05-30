0e2abb3872f1cb51eab36890d9ebcabb5b398889
David Hurng <dhurng@gmail.com>
2017-05-30 13:48:25 -0700

    added interview info

--- todo.py
+++ todo.py
@@ -12,7 +12,7 @@ can interact with it. Feel free to use whatever language and framework you're mo
 in memory, you don't have to use external storage. Don't spend too much time on this project skeleton - we will be 
 concentrating on what you accomplish during the call itself, so keep things simple.
 """
-import curses, datetime, time, operator
+import curses, datetime, operator, json
 from sys import argv
 
 
@@ -27,7 +27,7 @@ def get_param(prompt_string):
     screen.border(0)
     screen.addstr(2, 2, prompt_string)
     screen.refresh()
-    input = screen.getstr(10, 10, 60)
+    input = screen.getstr(10, 50, 60)
     return input
 
 
@@ -39,21 +39,44 @@ def display_list(list):
     """
     screen.clear()
     screen.border(0)
-    screen.addstr(1, 2, "Viewing List:")
+    screen.addstr(1, 2, "ITEM : STATUS : NOTES : PRIORITY")
     i = 2
     sorted_dict = sorted(list._map.iteritems(), key=operator.itemgetter(1))
     for pos, item in enumerate(sorted_dict):
-        screen.addstr(i, 2, "* %s" % sorted_dict[pos][0])
+        screen.addstr(i, 2, "* %s : %s : %s : %s" % (sorted_dict[pos][0], sorted_dict[pos][1][0], sorted_dict[pos][1][2], sorted_dict[pos][1][3]))
         i += 1
     screen.refresh()
-    dummy = screen.getstr(10, 10, 60)
+    dummy = screen.getstr(10, 50, 60)
+
+def display_test(map):
+    for i, j in enumerate(map.iteritems()):
+
+        print "HERE", i , j
+        print "***"
+        print j[0], j[1]
+
+def display_priority(map):
+    """
+    """
+    screen.clear()
+    screen.border(0)
+    screen.addstr(1, 2, "Priority : Count")
+    pos = 2
+
+    for i, j in enumerate(map.iteritems()):
+        screen.addstr(pos, 2, "* %s %s" % (j[0], j[1]))
+        pos += 1
+
+    screen.refresh()
+    dummy = screen.getstr(10, 50, 60)
+
 
 """*** To Do List Object ***"""
 class ToDoList(object):
     def __init__(self):
         self._map = {}
+        self.load_in()
 
-    # what about reading/writing out to a file?
     def add_item(self, item):
         """
         Add an item with (status, timestamp, notes) into list
@@ -62,7 +85,7 @@ class ToDoList(object):
         """
         exact_time = datetime.datetime.now()
         format_time = exact_time.strftime("%Y-%m-%d %H:%M:%S")
-        self._map[item] = ("New", format_time, "")
+        self._map[item] = ["NEW", format_time, "", "1"]
 
     def delete_item(self, item):
         """
@@ -72,27 +95,87 @@ class ToDoList(object):
         """
         self._map.pop(item, None)
 
+    def add_priority(self, priority, item):
+        trgt = self._map.get(item)
+        trgt[3] = priority
+
+    def priority_counter(self):
+        """
+        :return: 
+        """
+        priority_map = {}
+
+        for i in self._map.iteritems():
+            print "I", i
+            priority = i[1][3]
+            print "priority", priority
+
+            if priority not in priority_map:
+                priority_map[priority] = 1
+            else:
+                priority_map[priority] += 1
+
+        # for j in priority_map.iteritems():
+        #     print j
+        #     print j[0], j[1]
+
+        print "MAP", priority_map
+
+        return priority_map
+
     def write_out(self):
+        """
+        Append to existing file
+        :param item: str
+        :return: none
+        """
         path = './list.txt'
-        with open(path, "a") as file:
-            for i in self._map:
-                file.write(i + "\n")
-        file.close()
+        data = self._map
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
+        outfile.close()
+
+    def load_in(self):
+        """
+        Load the data from target file
+        :return: none
+        """
+        try:
+            path = './list.txt'
+            with open(path) as data_file:
+                data = json.load(data_file)
+            self._map = data
+        except:
+            print "Failure in loading existing file"
+
 
 """*** Execute program here ***"""
 my_list = ToDoList()
+#
+# my_list.add_item("test")
+# my_list.add_item("tester")
+# my_list.add_item("testerer")
+#
+# my_list.add_priority("2", "test")
+#
+# res = my_list.priority_counter()
+#
+# display_priority(res)
+
 x = 0
-while x != ord('4'):
+while x != ord('6'):
     screen = curses.initscr()
 
     screen.clear()
     screen.border(0)
     screen.addstr(1, 2, "* To-Do Console Application *")
-    screen.addstr(2, 2, "Please select an option buddy")
+    screen.addstr(2, 2, "Please Select An Option Buddy")
     screen.addstr(4, 4, "1 - Add Item")
     screen.addstr(5, 4, "2 - Delete Item")
     screen.addstr(6, 4, "3 - View Records")
-    screen.addstr(7, 4, "4 - Get Me Out Of Here!")
+    screen.addstr(7, 4, "4 - Add Priority")
+    screen.addstr(8, 4, "5 - View Priority List")
+    screen.addstr(9, 4, "6 - Get Me Out Of Here!")
     screen.refresh()
 
     x = screen.getch()
@@ -102,12 +185,24 @@ while x != ord('4'):
         my_list.add_item(item)
         curses.endwin()
     if x == ord('2'):
-        item = get_param("Select item to delete")
+        item = get_param("Enter item to delete")
         my_list.delete_item(item)
         curses.endwin()
     if x == ord('3'):
         display_list(my_list)
         curses.endwin()
+
+    if x == ord('4'):
+        item = get_param("Enter target item")
+        priority = get_param("Enter Priority to selected item")
+        my_list.add_priority(priority, item)
+        curses.endwin()
+
+    if x == ord('5'):
+        priority_map = my_list.priority_counter()
+        display_priority(priority_map)
+        curses.endwin()
+
 
 my_list.write_out()
 curses.endwin()
@@ -116,4 +211,12 @@ curses.endwin()
 If list out of bounds it will break 
 Cursor shows during display due to prompt dummy
 Only uses 1 list instead of allowing to create multiple
+
+Optional features:
+    Mark status with only 'inprogress, done' (if marked done, leave in datastructure for timestamp of arbitrary time)
+    Add notes to items
+    Prompt when delete is successful or failed
+    Indexing the map so you can delete it without typos
+    Hard coded the file path
+    Adding to prompt for duplicate
 """
